ecc02e7d4407d89c4c0cf6d5d862b02613e7559a
markus <markusklems@gmail.com>
2012-05-29 05:54:28 -0700

    YCSB deployment+installer features

--- services/ycsb/src/main/java/org/apache/whirr/service/ycsb/YcsbClusterActionHandler.java
+++ services/ycsb/src/main/java/org/apache/whirr/service/ycsb/YcsbClusterActionHandler.java
@@ -17,16 +17,15 @@ import org.apache.whirr.service.FirewallManager.Rule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Function;
 import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
+
 
 public class YcsbClusterActionHandler extends ClusterActionHandlerSupport {
 
 	  private static final Logger LOG =
 			    LoggerFactory.getLogger(YcsbClusterActionHandler.class);
 			    
-			  public static final String YCSB_ROLE = "ycsb";
+			  public static final String YCSB_ROLE = "ycsb-client";
 			  //public static final int YCSB_PORT = xxx;
 			  public static final int HTTP_PORT = 80;
 			  
@@ -40,51 +39,65 @@ public class YcsbClusterActionHandler extends ClusterActionHandlerSupport {
 			    return YCSB_ROLE;
 			  }
 
-			  protected Configuration getConfiguration(ClusterSpec spec)
-			    throws IOException {
-			    return getConfiguration(spec, "whirr-ycsb-default.properties");
-			  }
-
-			  /**
-			   * Install openjdk and YCSB.
-			   */
 			  @Override
 			  protected void beforeBootstrap(ClusterActionEvent event) throws IOException {
 			    ClusterSpec clusterSpec = event.getClusterSpec();
-			    Configuration config = getConfiguration(clusterSpec);
-
-			    addStatement(event, call(getInstallFunction(config, "java", "install_openjdk")));
-			    addStatement(event, call("install_tarball"));
-
-			    String tarball = prepareRemoteFileUrl(event, config.getString(BIN_TARBALL, null));
-			    String major = config.getString(MAJOR_VERSION, null);
-
-			    if (tarball != null && major != null) {
-			      addStatement(event, call("install_ycsb", major, tarball));
-			    } else {
-			      addStatement(event, call("install_ycsb"));
-			    }
+			    Configuration conf = clusterSpec.getConfiguration();
+			    // ...
 			  }
 
 			  @Override
-			  protected void beforeConfigure(ClusterActionEvent event) throws IOException, InterruptedException {
-			    event.getFirewallManager().addRule(
-			        Rule.create().destination(role(YCSB_ROLE)).ports(HTTP_PORT)
-			    );
+			  protected void beforeConfigure(final ClusterActionEvent event)
+			    throws IOException, InterruptedException {
+			    Cluster cluster = event.getCluster();
+			    // ...
 			  }
 			  
-			  static List<String> getHosts(Set<Instance> instances) {
-			      return Lists.transform(Lists.newArrayList(instances),
-			          new Function<Instance, String>() {
-			        @Override
-			        public String apply(Instance instance) {
-			          try {
-			            return instance.getPublicHostName();
-			          } catch (IOException e) {
-			            throw new IllegalArgumentException(e);
-			          }
-			        }
-			      });
-			    }
+//			  protected Configuration getConfiguration(ClusterSpec spec)
+//			    throws IOException {
+//			    return getConfiguration(spec, "whirr-ycsb-default.properties");
+//			  }
+//
+//			  /**
+//			   * Install openjdk and YCSB.
+//			   */
+//			  @Override
+//			  protected void beforeBootstrap(ClusterActionEvent event) throws IOException {
+//			    ClusterSpec clusterSpec = event.getClusterSpec();
+//			    Configuration config = getConfiguration(clusterSpec);
+//
+//			    addStatement(event, call(getInstallFunction(config, "java", "install_openjdk")));
+//			    addStatement(event, call("install_tarball"));
+//
+//			    String tarball = prepareRemoteFileUrl(event, config.getString(BIN_TARBALL, null));
+//			    String major = config.getString(MAJOR_VERSION, null);
+//
+//			    if (tarball != null && major != null) {
+//			      addStatement(event, call("install_ycsb", major, tarball));
+//			    } else {
+//			      addStatement(event, call("install_ycsb"));
+//			    }
+//			  }
+//
+//			  @Override
+//			  protected void beforeConfigure(ClusterActionEvent event) throws IOException, InterruptedException {
+//			    event.getFirewallManager().addRule(
+//			        Rule.create().destination(role(YCSB_ROLE)).ports(HTTP_PORT)
+//			    );
+//			  }
+//			  
+//			  static List<String> getHosts(Set<Instance> instances) {
+//			      return Lists.transform(Lists.newArrayList(instances),
+//			          new Function<Instance, String>() {
+//			        @Override
+//			        public String apply(Instance instance) {
+//			          try {
+//			            return instance.getPublicHostName();
+//			          } catch (IOException e) {
+//			            throw new IllegalArgumentException(e);
+//			          }
+//			        }
+//			      });
+//			    }
 
 }
