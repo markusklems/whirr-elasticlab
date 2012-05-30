82689b035328e85fe4c5c423558e6d42679ba5bc
markus <markusklems@gmail.com>
2012-05-30 10:23:41 -0700

    Creates a usertable for YCSB.

---
+++ services/cassandra/src/main/resources/functions/configure_ycsb.sh
@@ -0,0 +1,30 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+function configure_ycsb() {
+  #TODO ping cassandra status to see when it is running or time-out after 60 sec
+  sleep 60
+
+  CREATE_TABLE_STATEMENTS=/usr/local/create_usertable
+
+  cat >$CREATE_TABLE_STATEMENTS <<END_OF_FILE
+create keyspace usertable with strategy_options = [{replication_factor:3}] and placement_strategy = 'org.apache.cassandra.locator.SimpleStrategy';
+use usertable;
+create column family data with comparator='AsciiType';
+END_OF_FILE
+  
+  /usr/local/apache-cassandra-1.1.0/bin/cassandra-cli -f $CREATE_TABLE_STATEMENTS
+}
